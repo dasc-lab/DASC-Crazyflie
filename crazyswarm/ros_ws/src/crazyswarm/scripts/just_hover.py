ac6e816a3e60b9e90e8309b8ddf769f4d12c62bf
Haejoon Lee <5217232@naver.com>
2025-04-21 15:45:18 -0400

    Changed the just_hover

--- crazyswarm/ros_ws/src/crazyswarm/scripts/just_hover.py
+++ crazyswarm/ros_ws/src/crazyswarm/scripts/just_hover.py
@@ -13,9 +13,8 @@ def main():
     # Until the status is turned False, the drones keep moving to -x direction
     for cf in swarm.allcfs.crazyflies:
         cf.takeoff(targetHeight=HEIGHT, duration=HEIGHT / Z_SPEED)
-        timeHelper.sleep(HEIGHT / Z_SPEED)
+    timeHelper.sleep(HEIGHT / Z_SPEED)
     while True:
-        swarm.allcfs.goTo(np.array([-0.1,0.0,0.0]),0.0, 0.4)
         timeHelper.sleep(0.4)
         if not swarm.status:
             break
